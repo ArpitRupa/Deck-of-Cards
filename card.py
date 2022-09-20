2264bc7a5bf53d75febb68ef67160501dbdf69d0
Archit Rupakhetee <ar9cd@virginia.edu>
2022-09-20 18:23:05 -0400

    added merge sort for deck by value, still need to do suit

--- card.py
+++ card.py
@@ -1,14 +1,18 @@
 class Card():
 
-  def __init__(self, suit, value):
+  def __init__(self, suit, value, suitweight):
     self.suit = suit
     self.value = value
+    self.suitWeight = suitweight
 
   def get_card_value(self):
     return self.value
   
   def get_card_suit(self):
     return self.suit
+
+  def get_suit_weight(self):
+    return self.suitWeight
 
   def __str__(self):
     return ( str(self.get_card_value()) + " of " + str(self.get_card_suit()) )
